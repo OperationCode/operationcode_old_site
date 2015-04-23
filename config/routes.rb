3b5ee4f2d14babdc1f459a548dfd4250140670e4
David Molina <david.c.molina@gmail.com>
2015-04-22 22:50:57 -0700

    Add Donate link on header and redirect to Cogostar

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
-  
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  
+
   resources :veterans
 
 
   get "news" => "pages#news"
   get "events" => "pages#events"
   get "team" => "pages#team"
-  get "donate" => "pages#donate"
   get "founder" => "pages#founder"
   get "hackathons" => "pages#hackathons"
   get "contact" => "pages#contact"
@@ -17,10 +16,11 @@ Rails.application.routes.draw do
   get "learn" => "pages#learn"
   get "faq" => "pages#faq"
   get "about" => "pages#about"
-  
+
   #get "/action" => redirect("http://operationcode.launchrock.co/") #JDavis: capturing this information in-house vs launchrock
   get "action" => "veterans#new"
   get "/launchparty" => redirect("http://www.eventbrite.com/e/operation-code-launch-party-tickets-16494832488")
-  
+  get "/donate" => redirect("https://www.cogostar.org/projects/operationcode")
+
   root "pages#home"
 end
