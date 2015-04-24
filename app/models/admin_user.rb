936b2fe47f565004e0bb538375039f07681666dd
David Molina <david.c.molina@gmail.com>
2015-04-24 03:50:30 -0700

    Revised contact form, fixed footer

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -20,10 +20,10 @@
 class AdminUser < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 end
