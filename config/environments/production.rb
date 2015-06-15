f641f0752548e3deea3762b15e3c186d006654bd
David Molina <david.c.molina@gmail.com>
2015-06-14 19:42:37 -0700

    Fixed production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,10 +13,10 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  
+
   # JDavis: email settings for Sendgrid
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {  
+  config.action_mailer.smtp_settings = {
     address:              'smtp.sendgrid.net',
     port:                 '587',
     user_name:            ENV['SENDGRID_USERNAME'],
@@ -24,7 +24,7 @@ Rails.application.configure do
     domain:               'operationcode.org',
     authentication:       :plain,
     enable_starttls_auto: true
-    
+
   }
   config.action_mailer.default_url_options = { host: 'www.operationcode.org' }
 
@@ -41,7 +41,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
