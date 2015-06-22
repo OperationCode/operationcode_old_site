ef5d13c5648e9866855f93366a2c767879153a62
Glenn Espinosa <glennpeter.espinosa@gmail.com>
2015-06-22 04:42:37 -0400

    Fix donation page styling, add mailer, and flash messages

---
+++ app/mailers/donation_mailer.rb
@@ -0,0 +1,8 @@
+class DonationMailer < ActionMailer::Base
+  default from: "contact@operationcode.org"
+
+  def thankyou(donator)
+    @donator = donator
+    mail to: @donator.email, subject: "Thank you for donating to Operation Code!"
+  end
+end
