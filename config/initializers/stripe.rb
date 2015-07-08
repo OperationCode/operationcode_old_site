4e57cc2ca8440c4ffdaf840d35da4c709ce07fcb
cenderin <ml.alexander87@gmail.com>
2015-07-07 19:32:25 -0700

    hound fixes, hashes and spacing

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,12 +1,12 @@
 if Rails.env == 'production'
   Rails.configuration.stripe = {
-      :publishable_key => ENV['PUBLISHABLE_KEY'],
-      :secret_key => ENV['SECRET_KEY']
+    publishable_key: ENV['PUBLISHABLE_KEY'],
+    secret_key: ENV['SECRET_KEY']
   }
 else
   Rails.configuration.stripe = {
-      :publishable_key =>"pk_test_NdRxWpMYqwRbDFlOk5CvZcsP",
-      :secret_key => "sk_test_PFODeN8zX90T0qPTJf7Zqutm"
+    publishable_key: "pk_test_NdRxWpMYqwRbDFlOk5CvZcsP",
+    secret_key: "sk_test_PFODeN8zX90T0qPTJf7Zqutm"
   }
 end
 
