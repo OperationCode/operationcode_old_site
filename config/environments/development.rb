473bcab0e6afb36bf7721bc4fbc7d649737664c9
David Molina <david.c.molina@gmail.com>
2015-07-18 14:05:11 -0700

    Prepare Rails app for SSL

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,10 +15,10 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-  
+
   # JDavis: adding this for Devise.
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  
+
   # JDavis: using letter_opener for viewing emails in development
   config.action_mailer.delivery_method = :letter_opener
 
@@ -40,4 +40,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  config.force_ssl = true
 end
