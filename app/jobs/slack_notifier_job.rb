d6f669918755744c66a8853b029b15abe6aed044
RickR <rrein@akamai.com>
2016-03-10 23:39:27 -0500

    Notify slack channel when donation is saved

---
+++ app/jobs/slack_notifier_job.rb
@@ -0,0 +1,23 @@
+require 'slack/client'
+
+class SlackNotifierJob < ActiveJob::Base
+  queue_as :default
+
+  rescue_from do |exception|
+    Rails.logger.debug("Caught exception: #{exceptin.inspect}")
+  end
+
+  def perform(message)
+    res = slack_client.post_message_to channel: '#testing', with_text: message
+    Rails.logger.debug("Notified channel: #{res}")
+  end
+
+  private
+
+  def slack_client
+    @slack_client ||= Slack::Client.new(
+      subdomain: ENV.fetch("SLACK_SUBDOMAIN"),
+      token:     ENV.fetch("SLACK_TOKEN")
+    )
+  end
+end
