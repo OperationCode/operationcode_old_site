879a77879bf8a984d9733c7784407d78ae5cd7c9
RickR <rrein@akamai.com>
2016-03-30 18:12:07 -0400

    Only generate blog in prod

--- config/initializers/blog.rb
+++ config/initializers/blog.rb
@@ -1,23 +1,28 @@
-#See http://www.sitepoint.com/jekyll-rails/
+# See http://www.sitepoint.com/jekyll-rails/
 
 Rails.application.config.after_initialize do
   Rails.logger = Logger.new(STDOUT)
-  begin
-    #make a spot for the site
-    dest = Rails.root.join('public/blog')
+  if Rails.env.production?
+    begin
+      # make a spot for the site
+      dest = Rails.root.join('public/blog')
 
-    #generate the site
-    Jekyll::Site.new(
-    Jekyll.configuration({
-      "config" => Rails.root.join('config', 'jekyll.yml'),
-      "source" => Rails.root.join('blog').to_s,
-      "destination" => dest.to_s
-      })
-    ).process
+      # generate the site
+      Jekyll::Site.new(
+        Jekyll.configuration(
+          'config' => Rails.root.join('config', 'jekyll.yml'),
+          'source' => Rails.root.join('blog').to_s,
+          'destination' => dest.to_s
+        )
+      ).process
 
-    #this code gives the output color
-    Rails.logger.info "\e[0;32;49mJekyll site built!\e[0m]]"
-  rescue => e
-    Rails.logger.error "\e[0;31;49mJekyll site build failed.\e[0m\n\e[0;33;49mError:\e[0m #{e}"
+      # this code gives the output color
+      Rails.logger.info "\e[0;32;49mJekyll site built!\e[0m]]"
+    rescue => e
+      Rails.logger.error(
+        "\e[0;31;49mJekyll site build failed. " \
+        "\e[0m\n\e[0;33;49mError:\e[0m #{e}"
+      )
+    end
   end
 end
