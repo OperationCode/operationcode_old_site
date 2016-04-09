a3fd2141fadbdda9e5790bf979ff0398f910b8af
hollomancer <chollomon@gmail.com>
2016-04-09 12:58:55 -0400

    remove development/test GA tags & update Gemfile.lock

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,7 +36,4 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
-  # Separate Google Analytics tracker for each environment
-  GA.tracker = "UA-75642413-2"
 end
