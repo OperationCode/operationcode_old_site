65df74779e19e29e43e1239911158d92589cff7f
RickR <rrein@akamai.com>
2016-06-22 22:39:07 -0400

    move mail inviter to a job

--- app/controllers/veterans/registrations_controller.rb
+++ app/controllers/veterans/registrations_controller.rb
@@ -1,38 +1,38 @@
-class Veterans::RegistrationsController < Devise::RegistrationsController
-# before_filter :configure_sign_up_params, only: [:create]
-# before_filter :configure_account_update_params, only: [:update]
-  before_action :set_mentor_types, only: [:new, :create]
+class Veterans
+  class RegistrationsController < Devise::RegistrationsController
+    before_action :set_mentor_types, only: [:new, :create]
 
-  # GET /resource/sign_up
-  def new
-    super
-  end
+    # GET /resource/sign_up
+    def new
+      super
+    end
 
-  # POST /resource
-  def create
-    @veteran = Veteran.new(veteran_params)
-    if @veteran.save
-      send_notifications
-      redirect_to @veteran
-   else
-     render :new
-   end
-  end
+    # POST /resource
+    def create
+      @veteran = Veteran.new(veteran_params)
+      if @veteran.save
+        send_notifications
+        redirect_to @veteran
+      else
+       render :new
+      end
+    end
 
-  private
+    private
 
-  def set_mentor_types
-    @mentor_types = %w(Ruby/Rails Javascript Mobile Not\ Sure)
-  end
+    def set_mentor_types
+      @mentor_types = %w(Ruby/Rails Javascript Mobile Not\ Sure)
+    end
 
-  def veteran_params
-    params.require(:veteran).permit(:first_name, :last_name, :email, :zip, :service_branch, :request_mentor, :password, :password_confirmation)
-  end
+    def veteran_params
+      params.require(:veteran).permit(:first_name, :last_name, :email, :zip, :service_branch, :request_mentor, :password, :password_confirmation)
+    end
 
-  def send_notifications
-    UserMailer.welcome(@veteran).deliver_now
-    @veteran.send_slack_invitation
-    @veteran.send_mentor_request unless veteran_params[:request_mentor].blank?
-    @veteran.add_to_mailchimp
+    def send_notifications
+      UserMailer.welcome(@veteran).deliver_now
+      @veteran.send_slack_invitation
+      @veteran.send_mentor_request unless veteran_params[:request_mentor].blank?
+      @veteran.add_to_mailchimp
+    end
   end
 end
