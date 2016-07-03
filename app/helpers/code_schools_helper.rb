722e9824073fef5141bb549745dd8eae99892570
RickR <rrein@akamai.com>
2016-07-03 01:20:14 -0400

    unused

--- app/helpers/code_schools_helper.rb
+++
@@ -1,2 +0,0 @@
-module CodeSchoolsHelper
-end
--- test/controllers/code_schools_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class CodeSchoolsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
