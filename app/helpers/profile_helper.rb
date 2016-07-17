03d7143e6e27c9f535b5499de7ae5f5917e21b92
RickR <rrein@akamai.com>
2016-07-17 00:12:46 -0400

    code climate

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 module ProfileHelper
   def menu_item(item_name:, item_path:)
     content_tag(:li, link_to(item_name, item_path, target: :_self), class: :tab)
--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class Mentor < Veteran
   has_many :mentees, class_name: :Veteran, foreign_key: :mentor_id
 
