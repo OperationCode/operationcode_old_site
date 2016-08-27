6aa63d0b6122ab6b9ab8908c481b46ffc6735ccd
RickR <rrein@akamai.com>
2016-08-27 10:46:59 -0400

    add to home page

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,13 @@
 module PagesHelper
+  def resource_name
+    :veteran
+  end
+
+  def resource
+    @resource ||= Veteran.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:veteran]
+  end
 end
