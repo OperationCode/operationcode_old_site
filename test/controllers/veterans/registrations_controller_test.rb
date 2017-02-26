3cdcedb4d3d6dd8e197a5a34fd91fbe64db77777
Lee Saville <l.w.saville@gmail.com>
2017-02-26 07:31:41 -0700

    Change veteran registration test to expect the redirect to go to 'thanks'

--- test/controllers/veterans/registrations_controller_test.rb
+++ test/controllers/veterans/registrations_controller_test.rb
@@ -7,7 +7,7 @@ module Veterans
     test 'a veteran can register' do
       @request.env['devise.mapping'] = Devise.mappings[:veteran]
       post :create, veteran: { email: 'test@example.com', zip: '11772' }
-      assert_redirected_to '/'
+      assert_redirected_to '/thanks'
     end
   end
 end
