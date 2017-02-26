a257f9d336373b460c0fcfa435c31a8daa8474c2
Lee Saville <l.w.saville@gmail.com>
2017-02-26 08:02:54 -0700

    Fix typo in path expectation

--- test/controllers/veterans/registrations_controller_test.rb
+++ test/controllers/veterans/registrations_controller_test.rb
@@ -7,7 +7,7 @@ module Veterans
     test 'a veteran can register' do
       @request.env['devise.mapping'] = Devise.mappings[:veteran]
       post :create, veteran: { email: 'test@example.com', zip: '11772' }
-      assert_redirected_to '/thanks'
+      assert_redirected_to '/veterans/thanks'
     end
   end
 end
