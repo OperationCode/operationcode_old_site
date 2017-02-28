3a00d33716a70167e6a8d1912b1ec77c9de31821
Lee Saville <l.w.saville@gmail.com>
2017-02-28 08:18:30 -0700

    Create PORO to handle the dynamic generation of slack download url based on the users os

---
+++ app/models/slack_url.rb
@@ -0,0 +1,12 @@
+class SlackUrl
+  def self.get_url(os)
+    options = Hash.new("slack unsupported os")
+    download_options = {
+      'Mac' => "https://slack.com/ssb/download-osx",
+      'Windows' => "https://slack.com/ssb/download-win",
+      'Linux' => "https://downloads.slack-edge.com/linux_releases/slack-desktop-2.4.2-amd64.deb"
+    }
+    options.merge!(download_options)
+    options[os]
+  end
+end
