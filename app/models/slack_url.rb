ab08821966b37a45f3aed3b849ef901651b262cd
Lee Saville <l.w.saville@gmail.com>
2017-02-28 14:16:00 -0700

    Change double quotes to single

--- app/models/slack_url.rb
+++ app/models/slack_url.rb
@@ -1,10 +1,10 @@
 class SlackUrl
   def self.get_url(os)
-    options = Hash.new("slack unsupported os")
+    options = Hash.new('slack unsupported os')
     download_options = {
-      'Mac' => "https://slack.com/ssb/download-osx",
-      'Windows' => "https://slack.com/ssb/download-win",
-      'Linux' => "https://downloads.slack-edge.com/linux_releases/slack-desktop-2.4.2-amd64.deb"
+      'Mac' => 'https://slack.com/ssb/download-osx',
+      'Windows' => 'https://slack.com/ssb/download-win',
+      'Linux' => 'https://downloads.slack-edge.com/linux_releases/slack-desktop-2.4.2-amd64.deb'
     }
     options.merge!(download_options)
     options[os]
