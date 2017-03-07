d8932ee1fc1a78e0a493041d1ca9570b41fab550
Lee Saville <l.w.saville@gmail.com>
2017-03-07 08:05:00 -0700

    Change hash syntax

--- app/models/slack_url.rb
+++ app/models/slack_url.rb
@@ -1,10 +1,11 @@
+# frozen_string_literal: true
 class SlackUrl
   def self.get_url(os)
     download_options = {
-      :mac     => 'https://slack.com/ssb/download-osx',
-      :windows => 'https://slack.com/ssb/download-win',
-      :ubuntu  => 'https://downloads.slack-edge.com/linux_releases/slack-desktop-2.4.2-amd64.deb',
-      :fedora  => 'https://downloads.slack-edge.com/linux_releases/slack-2.5.1-0.1.fc21.x86_64.rpm'
+      mac:     'https://slack.com/ssb/download-osx',
+      windows: 'https://slack.com/ssb/download-win',
+      ubuntu:  'https://downloads.slack-edge.com/linux_releases/slack-desktop-2.4.2-amd64.deb',
+      fedora:  'https://downloads.slack-edge.com/linux_releases/slack-2.5.1-0.1.fc21.x86_64.rpm'
     }
     download_options[os]
   end
