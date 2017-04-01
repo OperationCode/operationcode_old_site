0ab7e218fa1af13e98255925179fe9d40847a986
tonyvince <tonyvince7@gmail.com>
2017-04-01 14:37:29 +0530

    Rescue GibbonError

--- app/jobs/mailchimp_inviter_job.rb
+++ app/jobs/mailchimp_inviter_job.rb
@@ -2,15 +2,19 @@ class MailchimpInviterJob < ActiveJob::Base
   queue_as :default
 
   def perform(email:, first_name:, last_name:)
-    gibbon = Gibbon::Request.new(api_key: OperationCode.fetch_secret_with(name: :mailchimp_api_key))
-    list_id = OperationCode.fetch_secret_with name: :mailchimp_list_id
+    begin
+      gibbon = Gibbon::Request.new(api_key: OperationCode.fetch_secret_with(name: :mailchimp_api_key))
+      list_id = OperationCode.fetch_secret_with name: :mailchimp_list_id
 
-    gibbon.lists(list_id).members.create(
-      body: {
-        email_address: email,
-        status: 'subscribed',
-        merge_fields: { FNAME: first_name, LNAME: last_name }
-      }
-    )
+      gibbon.lists(list_id).members.create(
+        body: {
+          email_address: email,
+          status: 'subscribed',
+          merge_fields: { FNAME: first_name, LNAME: last_name }
+        }
+      )
+    rescue Gibbon::GibbonError=> e
+      logger.info "There is a problem with Mailchimp Invite: #{e.message}"
+    end
   end
 end
