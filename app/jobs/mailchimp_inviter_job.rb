1b4dc7d420adc8f360f92ca1bdd72f74c629950c
tonyvince <tonyvince7@gmail.com>
2017-04-03 09:55:32 +0530

    Remove redundant begin and add proper spacing

--- app/jobs/mailchimp_inviter_job.rb
+++ app/jobs/mailchimp_inviter_job.rb
@@ -2,19 +2,17 @@ class MailchimpInviterJob < ActiveJob::Base
   queue_as :default
 
   def perform(email:, first_name:, last_name:)
-    begin
-      gibbon = Gibbon::Request.new(api_key: OperationCode.fetch_secret_with(name: :mailchimp_api_key))
-      list_id = OperationCode.fetch_secret_with name: :mailchimp_list_id
+    gibbon = Gibbon::Request.new(api_key: OperationCode.fetch_secret_with(name: :mailchimp_api_key))
+    list_id = OperationCode.fetch_secret_with name: :mailchimp_list_id
 
-      gibbon.lists(list_id).members.create(
-        body: {
-          email_address: email,
-          status: 'subscribed',
-          merge_fields: { FNAME: first_name, LNAME: last_name }
-        }
-      )
-    rescue Gibbon::GibbonError=> e
-      logger.info "There is a problem with Mailchimp Invite: #{e.message}"
-    end
+    gibbon.lists(list_id).members.create(
+      body: {
+        email_address: email,
+        status: 'subscribed',
+        merge_fields: { FNAME: first_name, LNAME: last_name }
+      }
+    )
+    rescue Gibbon::GibbonError => e
+      logger.error "There is a problem with Mailchimp Invite: #{e.message}"
   end
 end
