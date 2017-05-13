cef5eb9e3c27fd3d52359eb55e2278074472f3b0
Hamel Desai <hdesai719@gmail.com>
2017-05-13 10:06:16 -0400

    Took out a put statement

--- app/controllers/code_schools_controller.rb
+++ app/controllers/code_schools_controller.rb
@@ -12,8 +12,6 @@ class CodeSchoolsController < ApplicationController
     code_schools = CodeSchools.new.all
     @lat_longs = []
 
-    puts "first code school #{code_schools[0]}"
-
     code_schools.each do |hash| 
       if hash.key?("latitude") && hash.key?("longitude")
         @lat_longs << [hash["longitude"], hash["latitude"]]
